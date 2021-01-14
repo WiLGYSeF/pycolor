fec252af442a16a0e75ab28ca3475b1704aadc38
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-01-14 18:11:13 -0500

    add arg tests

---
+++ tests/test_main.py
@@ -0,0 +1,94 @@
+import unittest
+
+import main
+
+
+ARGV = 'argv'
+MY_ARGS = 'my_args'
+CMD_ARGS = 'cmd_args'
+START_IDX = 'start_idx'
+
+GET_MY_ARGS = [
+    {
+        ARGV: [],
+        MY_ARGS: [],
+        CMD_ARGS: [],
+        START_IDX: 0
+    },
+    {
+        ARGV: ['ls'],
+        MY_ARGS: [],
+        CMD_ARGS: ['ls'],
+        START_IDX: 0
+    },
+    {
+        ARGV: ['pycolor'],
+        MY_ARGS: [],
+        CMD_ARGS: [],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['ls', '-l'],
+        MY_ARGS: [],
+        CMD_ARGS: ['ls', '-l'],
+        START_IDX: 0
+    },
+    {
+        ARGV: ['pycolor', 'ls', '-l'],
+        MY_ARGS: [],
+        CMD_ARGS: ['ls', '-l'],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['pycolor', '--color', 'ls', '-l'],
+        MY_ARGS: ['--color'],
+        CMD_ARGS: ['ls', '-l'],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['pycolor', '--test', '--color', 'ls', '-l'],
+        MY_ARGS: ['--test', '--color'],
+        CMD_ARGS: ['ls', '-l'],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['pycolor', '--test', 'abc', '--color', 'ls', '-l'],
+        MY_ARGS: ['--test'],
+        CMD_ARGS: ['abc', '--color', 'ls', '-l'],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['pycolor', '--color', '--', 'ls', '-l'],
+        MY_ARGS: ['--color'],
+        CMD_ARGS: ['ls', '-l'],
+        START_IDX: 1
+    },
+    {
+        ARGV: ['pycolor', '--', '--weirdname'],
+        MY_ARGS: [],
+        CMD_ARGS: ['--weirdname'],
+        START_IDX: 1
+    },
+]
+
+GET_ARGS = {
+    'invalid': ValueError(),
+    '--asdf': ('asdf', None),
+    '--asdf=': ('asdf', ''),
+    '--asdf=test': ('asdf', 'test')
+}
+
+
+class MainTest(unittest.TestCase):
+    def test_get_my_args(self):
+        for entry in GET_MY_ARGS:
+            my_args, cmd_args = main.get_my_args(entry[ARGV], start_idx=entry[START_IDX])
+            self.assertListEqual(my_args, entry[MY_ARGS])
+            self.assertListEqual(cmd_args, entry[CMD_ARGS])
+
+    def test_get_arg(self):
+        for string, val in GET_ARGS.items():
+            if isinstance(val, Exception):
+                self.assertRaises(Exception, main.get_arg, string)
+            else:
+                self.assertTupleEqual(main.get_arg(string), val)
