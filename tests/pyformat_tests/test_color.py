a2266c348d87a4c8c0c0b90f626e72187cffe42e
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-01-14 10:38:37 -0500

    add 256 color and 24bit color tests

--- tests/pyformat_tests/test_color.py
+++ tests/pyformat_tests/test_color.py
@@ -1,6 +1,7 @@
 import unittest
 
 import pyformat
+import pyformat.color
 
 
 FORMAT_COLOR_STRINGS = {
@@ -11,7 +12,19 @@ FORMAT_COLOR_STRINGS = {
     '%(Cred)%(Cblue)': '\x1b[31m\x1b[34m',
     '%(Cred;^blue)': '\x1b[31;44m',
     '%C(red)abc': '\x1b[31mabc',
+    '%(C123)abc': '\x1b[38;5;123mabc',
+    '%(C0xffaa00)abc': '\x1b[38;2;255;170;0mabc',
     '%(Cunderline;red)abc%(C^underline)': '\x1b[4;31mabc\x1b[24m'
+}
+
+HEX_TO_RGBS = {
+    '0xffffff': (255, 255, 255),
+    '0xfff': (255, 255, 255),
+    'ffffff': (255, 255, 255),
+    'fff': (255, 255, 255),
+    '0xffaa00': (255, 170, 0),
+    '0xfa0': (255, 170, 0),
+    'invalid': ValueError()
 }
 
 
@@ -19,3 +32,10 @@ class ColorTest(unittest.TestCase):
     def test_format_color_string(self):
         for key, val in FORMAT_COLOR_STRINGS.items():
             self.assertEqual(pyformat.format_string(key), val)
+
+    def test_hex_to_rgb(self):
+        for key, val in HEX_TO_RGBS.items():
+            if isinstance(val, Exception):
+                self.assertRaises(Exception, pyformat.color.hex_to_rgb, key)
+            else:
+                self.assertTupleEqual(pyformat.color.hex_to_rgb(key), val)
