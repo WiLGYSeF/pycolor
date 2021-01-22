9d90ca633a75e9774f3d7d91af2a05a5f98029e2
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-01-22 11:10:47 -0500

    add execute() date test

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -1,8 +1,22 @@
-from io import BytesIO
+import datetime
 import unittest
 
-import pycolor
+import execute
 
 
+# TODO: more test cases
 class ExecuteTest(unittest.TestCase):
-	pass
+    def test_execute_date(self):
+        def stdout_cb(data):
+            datestr = data.decode('utf-8').rstrip('\n')
+
+            self.assertEqual(
+                datestr,
+                datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+            )
+
+        def stderr_cb(data):
+            self.assertTrue(False)
+
+        returncode = execute.execute(['date', '+%Y%m%d%H%M%S'], stdout_cb, stderr_cb)
+        self.assertEqual(returncode, 0)
