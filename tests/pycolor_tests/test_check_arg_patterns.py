e1873a880aa35ce710391209b6d69db13fa92eb0
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-01-23 22:19:14 -0500

    reorganize pycolor tests

--- tests/pycolor_tests/test_check_arg_patterns.py
+++ tests/pycolor_tests/test_check_arg_patterns.py
@@ -2,13 +2,10 @@ import unittest
 
 from pycolor_class import Pycolor
 
+
 ARGS = 'args'
 ARGPATTERNS = 'argpatterns'
 RESULT = 'result'
-
-ARGLEN = 'arglen'
-POSITION = 'position'
-RANGE = 'range'
 
 CHECK_ARG_PATTERNS = [
     {
@@ -121,91 +118,11 @@ CHECK_ARG_PATTERNS = [
     },
 ]
 
-GET_ARG_RANGES = [
-    {
-        ARGLEN: 4,
-        POSITION: None,
-        RANGE: [0, 1, 2, 3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '*',
-        RANGE: [0, 1, 2, 3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: 'blah',
-        RANGE: [0, 1, 2, 3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '1',
-        RANGE: [0],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '2',
-        RANGE: [1],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '3',
-        RANGE: [2],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '4',
-        RANGE: [3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '5',
-        RANGE: [],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '+2',
-        RANGE: [1, 2, 3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '-3',
-        RANGE: [0, 1, 2],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '>8',
-        RANGE: [],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: '<8',
-        RANGE: [0, 1, 2, 3],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: 8,
-        RANGE: [],
-    },
-    {
-        ARGLEN: 4,
-        POSITION: 3,
-        RANGE: [2],
-    },
-]
 
-
-class PycolorClassTest(unittest.TestCase):
+class CheckArgPatternsTest(unittest.TestCase):
     def test_check_arg_patterns(self):
         for entry in CHECK_ARG_PATTERNS:
             self.assertEqual(
                 Pycolor.check_arg_patterns(entry[ARGS], entry[ARGPATTERNS]),
                 entry[RESULT]
-            )
-
-    def test_get_arg_range(self):
-        for entry in GET_ARG_RANGES:
-            self.assertListEqual(
-                list(Pycolor.get_arg_range(entry[ARGLEN], entry[POSITION])),
-                entry[RANGE]
             )
