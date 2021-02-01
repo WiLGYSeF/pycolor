b01a236c46e76f773794fafb21ef8e813aab8109
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-02-01 14:13:01 -0500

    add df tests

---
+++ tests/execute_tests/df/test_df.py
@@ -0,0 +1,20 @@
+import os
+import unittest
+import unittest.mock
+
+from tests.execute_tests.helpers import check_pycolor_execute
+
+
+MOCKED_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mocked_data')
+
+
+class DfTest(unittest.TestCase):
+    def test_normal(self):
+        check_pycolor_execute(self, ['df', '-h'], MOCKED_DATA, 'normal')
+
+    def test_color_fields(self):
+        check_pycolor_execute(self, ['df', '-h'], MOCKED_DATA, 'color-fields')
+
+    def test_no_color_fields_T(self): #pylint: disable=invalid-name
+        check_pycolor_execute(self, ['df', '-Th'], MOCKED_DATA, 'no-color-fields-T')
+        check_pycolor_execute(self, ['df', '-h', '-T'], MOCKED_DATA, 'no-color-fields-T')
