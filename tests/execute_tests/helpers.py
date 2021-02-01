970a8420fcc5418fce2dc7e8c87648bd809908e0
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-02-01 12:50:36 -0500

    add write_output to check_pycolor_execute()

--- tests/execute_tests/helpers.py
+++ tests/execute_tests/helpers.py
@@ -8,7 +8,14 @@ from execute import read_stream
 import pycolor_class
 
 
-def check_pycolor_execute(self, cmd, mocked_data_dir, test_name, print_output=False):
+def check_pycolor_execute(
+    self,
+    cmd,
+    mocked_data_dir,
+    test_name,
+    print_output=False,
+    write_output=False
+):
     pycobj = pycolor_class.Pycolor(color_mode='always')
     filename_prefix = os.path.join(mocked_data_dir, test_name)
     pycobj.load_file(filename_prefix + '.json')
@@ -26,16 +33,16 @@ def check_pycolor_execute(self, cmd, mocked_data_dir, test_name, print_output=Fa
         except FileNotFoundError:
             return None
 
+    def write_file(fname, data):
+        with open(fname, 'w') as file:
+            file.write(data)
+            print('Wrote to ' + fname)
+
     stdout = open_fstream(filename_prefix + '.txt')
     stderr = open_fstream(filename_prefix + '.err.txt')
 
     output_expected = read_file(filename_prefix + '.out.txt')
     output_expected_err = read_file(filename_prefix + '.out.err.txt')
-
-    if output_expected is None and output_expected_err is None:
-        raise FileNotFoundError(
-            '%s is missing expected output test files for: %s' % (mocked_data_dir, test_name)
-        )
 
     pycobj.stdout = io.TextIOWrapper(io.BytesIO())
     pycobj.stderr = io.TextIOWrapper(io.BytesIO())
@@ -48,19 +55,31 @@ def check_pycolor_execute(self, cmd, mocked_data_dir, test_name, print_output=Fa
     if stderr is not None:
         stderr.close()
 
+    pycobj.stdout.seek(0)
+    data = pycobj.stdout.read()
+
+    if print_output:
+        print(data)
+    if write_output:
+        write_file(filename_prefix + '.out.txt', data)
+
     if output_expected is not None:
-        pycobj.stdout.seek(0)
-        data = pycobj.stdout.read()
-        if print_output:
-            print(data)
         self.assertEqual(data, output_expected)
+    else:
+        self.assertEqual(data, '')
+
+    pycobj.stderr.seek(0)
+    data = pycobj.stderr.read()
+
+    if print_output:
+        print(data)
+    if write_output:
+        write_file(filename_prefix + '.out.err.txt', data)
 
     if output_expected_err is not None:
-        pycobj.stderr.seek(0)
-        data = pycobj.stderr.read()
-        if print_output:
-            print(data)
         self.assertEqual(data, output_expected_err)
+    else:
+        self.assertEqual(data, '')
 
 @contextmanager
 def execute_patch(obj, stdout_stream, stderr_stream):
