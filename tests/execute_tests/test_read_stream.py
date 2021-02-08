cf84a295739c0e956703beecab9a649fdd794da5
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-02-08 14:55:20 -0500

    if data is a list, stream is fed next element after read_stream() to simulate data being written to buffer in between read calls

--- tests/execute_tests/test_read_stream.py
+++ tests/execute_tests/test_read_stream.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+import os
 import unittest
 
 from execute import read_stream
@@ -68,47 +69,72 @@ class ReadStreamTest(unittest.TestCase):
 
     def test_twoline_buflf_last(self):
         stream = StreamObj([
-            b'abc\n',
+            b'abc\n123\n',
             b'123\n'
         ])
 
         callback_data = stream.read_stream(buffer_line=True, last=True)
-        for i in range(len(stream.data)):
-            self.assertEqual(
-                callback_data[i],
-                stream.data[i].decode('utf-8')
-            )
+        self.assertEqual(
+            callback_data[0],
+            'abc\n'
+        )
+        self.assertEqual(
+            callback_data[1],
+            '123\n'
+        )
 
     def test_twoline_buflf(self):
         stream = StreamObj([
-            b'abc\n',
-            b'123\n'
+            b'abc\n123\n'
         ])
 
         callback_data = stream.read_stream(buffer_line=True, last=False)
-        for i in range(len(stream.data)):
-            self.assertEqual(
-                callback_data[i],
-                stream.data[i].decode('utf-8')
-            )
+        self.assertEqual(
+            callback_data[0],
+            'abc\n'
+        )
+        self.assertEqual(
+            callback_data[1],
+            '123\n'
+        )
+
+    def test_twoline_buflf_late_lf_one(self):
+        stream = StreamObj([
+            b'abc',
+            b'\n123\n'
+        ])
+
+        self.assertEqual(
+            stream.read_stream(buffer_line=True, last=False),
+            []
+        )
+        callback_data = stream.read_stream(buffer_line=True, last=False)
+        self.assertEqual(
+            callback_data[0],
+            'abc\n'
+        )
+        self.assertEqual(
+            callback_data[1],
+            '123\n'
+        )
 
 
 class StreamObj:
     def __init__(self, data, callback=None):
-        self.data = data
+        self.set_stream(data)
         self.callback_func = callback
 
-        self.set_stream(data)
         self.callback_data = ''
-
         self.last_callback_data = None
 
     def set_stream(self, data):
         self.stream = BytesIO()
+        self.data = data
+        self.data_idx = None
 
         if isinstance(data, list):
-            for segm in data:
-                self.stream.write(segm)
+            self.stream.write(data[0])
+            self.data_idx = 1
         else:
             self.stream.write(data)
 
@@ -123,6 +149,7 @@ class StreamObj:
 
     def read_stream(self, buffer_line=True, encoding='utf-8', last=False):
         self.last_callback_data = []
+
         read_stream(
             self.stream,
             self.callback,
@@ -130,4 +157,10 @@ class StreamObj:
             encoding=encoding,
             last=last
         )
+
+        if self.data_idx is not None and self.data_idx < len(self.data):
+            self.stream.write(self.data[self.data_idx])
+            self.stream.seek(-len(self.data[self.data_idx]), os.SEEK_CUR)
+            self.data_idx += 1
+
         return self.last_callback_data
