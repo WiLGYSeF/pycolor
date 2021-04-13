903e4e6f3e44fefe9c022bb574fb2d326b8bd5f8
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-04-13 16:51:35 -0400

    increase poll() sleep time again

--- execute.py
+++ execute.py
@@ -107,7 +107,7 @@ def execute(cmd, stdout_callback, stderr_callback, buffer_line=True, encoding='u
         while process.poll() is None:
             _read(process.stdout, stdout_callback)
             _read(process.stderr, stderr_callback)
-            time.sleep(0.001)
+            time.sleep(0.0001)
 
         _read(process.stdout, stdout_callback, last=True)
         _read(process.stdout, stdout_callback, last=True)
