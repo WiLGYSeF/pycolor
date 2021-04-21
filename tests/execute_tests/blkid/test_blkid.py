5dfa319c7be8d762b0cc9bc61638ba703addabcd
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-04-21 16:15:04 -0400

    add blkid tests

--- tests/execute_tests/blkid/test_blkid.py
+++ tests/execute_tests/blkid/test_blkid.py
@@ -11,3 +11,15 @@ MOCKED_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mocked_
 class BlkidTest(unittest.TestCase):
     def test_replace_groups_list(self):
         check_pycolor_execute(self, ['blkid'], MOCKED_DATA, 'replace-groups-list')
+
+    def test_replace_groups_list_overlap_before(self):
+        check_pycolor_execute(self, ['blkid'], MOCKED_DATA, 'replace-groups-list-overlap-before')
+
+    def test_replace_groups_list_overlap_after(self):
+        check_pycolor_execute(self, ['blkid'], MOCKED_DATA, 'replace-groups-list-overlap-after')
+
+    def test_replace_groups_list_overlap_before_soft(self):
+        check_pycolor_execute(self, ['blkid'], MOCKED_DATA, 'replace-groups-list-overlap-before-soft', print_output=True, debug=3)
+
+    def test_replace_groups_list_overlap_after_soft(self):
+        check_pycolor_execute(self, ['blkid'], MOCKED_DATA, 'replace-groups-list-overlap-after-soft', print_output=True, debug=3)
