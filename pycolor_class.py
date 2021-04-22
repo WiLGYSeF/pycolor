25944b5505f92d4967e10af7e15a99fbf4aed9b1
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-04-22 16:17:53 -0400

    move apply_pattern()

--- pycolor_class.py
+++ pycolor_class.py
@@ -4,15 +4,12 @@ import os
 import sys
 import tempfile
 
-from colorpositions import update_color_positions, insert_color_data, offset_color_positions
+from applypattern import apply_pattern
+from colorpositions import insert_color_data
 from colorstate import ColorState
 import execute
-from group_index import get_named_group_at_index
-from match_group_replace import match_group_replace
 from profileloader import ProfileLoader
 import pyformat
-from search_replace import search_replace, update_positions
-from split import re_split
 from which import which
 
 
@@ -99,21 +96,29 @@ class Pycolor:
         return retcode
 
     def data_callback(self, stream, data):
-        newdata = data
         color_positions = {}
         removed_newline = False
         removed_carriagereturn = False
 
-        if len(newdata) != 0 and newdata[-1] == '\n':
+        if len(data) != 0 and data[-1] == '\n':
             self.linenum += 1
-            newdata = newdata[:-1]
+            data = data[:-1]
             removed_newline = True
-        if len(newdata) != 0 and newdata[-1] == '\r':
-            newdata = newdata[:-1]
+        if len(data) != 0 and data[-1] == '\r':
+            data = data[:-1]
             removed_carriagereturn = True
 
         self.debug_print(4, 'on line %d', self.linenum)
-        self.debug_print(1, 'received: %s', newdata.encode('utf-8'))
+        self.debug_print(1, 'received: %s', data.encode('utf-8'))
+
+        color_positions = {}
+        context = {
+            'color': {
+                'enabled': self.is_color_enabled(),
+                'aliases': self.profloader.color_aliases,
+                'positions': color_positions,
+            }
+        }
 
         for pat in self.current_profile.patterns:
             if any([
@@ -123,11 +128,11 @@ class Pycolor:
             ]):
                 continue
 
-            matched, applied = self.apply_pattern(pat, newdata, color_positions)
+            matched, applied = apply_pattern(pat, self.linenum, data, context)
             if matched:
                 if pat.filter:
-                    self.debug_print(2, 'filtered: %s', newdata.encode('utf-8'))
-                    newdata = None
+                    self.debug_print(2, 'filtered: %s', data.encode('utf-8'))
+                    data = None
                     break
 
                 if self.debug >= 3:
@@ -135,25 +140,25 @@ class Pycolor:
                         insert_color_data(applied, color_positions).encode('utf-8')
                     )
 
-                newdata = applied
+                data = applied
                 if pat.skip_others:
                     break
 
-        if newdata is None:
+        if data is None:
             return
 
         if len(color_positions) != 0:
-            newdata = insert_color_data(newdata, color_positions)
+            data = insert_color_data(data, color_positions)
 
-        self.debug_print(2, 'writing:  %s', newdata.encode('utf-8'))
+        self.debug_print(2, 'writing:  %s', data.encode('utf-8'))
 
         if self.current_profile.timestamp:
             self.write_timestamp(stream)
 
         stream.flush()
-        stream.buffer.write(newdata.encode('utf-8'))
+        stream.buffer.write(data.encode('utf-8'))
 
-        self.color_state.set_state_by_string(newdata)
+        self.color_state.set_state_by_string(data)
 
         if self.current_profile.soft_reset_eol:
             stream.write(self.color_state_orig.get_string(
@@ -166,257 +171,6 @@ class Pycolor:
             stream.write('\n')
 
         stream.flush()
-
-    def apply_pattern(self, pat, data, color_positions):
-        if not pat.is_active(self.linenum, data):
-            return False, None
-        if pat.super_regex is not None and not pat.super_regex.search(data):
-            return False, None
-
-        context = {
-            'string': data,
-            'color': {
-                'enabled': self.is_color_enabled(),
-                'state': self.color_state,
-                'aliases': self.profloader.color_aliases,
-                'positions': color_positions,
-            }
-        }
-
-        if pat.separator_regex is not None:
-            fields = re_split(pat.separator_regex, data)
-            field_idxs = pat.get_field_indexes(fields)
-            context['fields'] = fields
-
-        if pat.separator_regex is None or all([
-            pat.field is not None,
-            pat.field == 0,
-            len(field_idxs) != 0
-        ]):
-            if pat.replace_all is not None:
-                match = pat.regex.search(data)
-                if match is None:
-                    return False, None
-
-                context['match'] = match
-                context['idx'] = match.start()
-
-                data, colorpos = pyformat.format_string(
-                    pat.replace_all,
-                    context=context,
-                    return_color_positions=True
-                )
-                color_positions.clear()
-                color_positions.update(colorpos)
-                return True, data
-            if pat.replace is not None:
-                data, replace_ranges, colorpos = self.pat_schrep(pat, data)
-                if len(replace_ranges) == 0:
-                    return False, None
-
-                update_positions(color_positions, replace_ranges)
-                update_color_positions(color_positions, colorpos)
-                return True, data
-            if 'fields' in context and all([
-                len(pat.replace_fields) != 0,
-                len(field_idxs) != 0
-            ]):
-                newdata = ''
-                changed = False
-                offset = 0
-                choffset = 0
-                replace_ranges = []
-                field_idx = 0
-
-                for idx in range(0, len(fields) + 1, 2):
-                    replace_val = None
-                    sep = fields[idx + 1] if idx != len(fields) - 1 else ''
-
-                    if isinstance(pat.replace_fields, dict):
-                        for key, val in pat.replace_fields.items():
-                            for num in key.split(','):
-                                start, end, step = pyformat.fieldsep.get_field_range(num, fields)
-                                start = pyformat.fieldsep.idx_to_num(start)
-                                end = pyformat.fieldsep.idx_to_num(end)
-
-                                if field_idx in range(start - 1, end, step):
-                                    replace_val = val
-                                    changed = True
-                    elif isinstance(pat.replace_fields, list) and field_idx < len(pat.replace_fields):
-                        replace_val = pat.replace_fields[field_idx]
-                        changed = True
-
-                    if replace_val is None:
-                        replace_val = fields[idx]
-
-                    context['field_cur'] = fields[idx]
-                    replace_val, colorpos = pyformat.format_string(
-                        replace_val,
-                        context=context,
-                        return_color_positions=True
-                    )
-
-                    colorpos = offset_color_positions(colorpos, offset)
-                    choffset += len(replace_val) - len(fields[idx])
-
-                    update_positions(colorpos, replace_ranges)
-                    replace_ranges.append((
-                        (offset, offset + len(fields[idx])),
-                        (
-                            offset + choffset,
-                            offset + choffset + len(replace_val)
-                        )
-                    ))
-                    update_color_positions(color_positions, colorpos)
-
-                    newdata += replace_val + sep
-                    offset += len(replace_val) + len(sep)
-                    field_idx += 1
-
-                return changed, newdata
-            if len(pat.replace_groups) != 0:
-                choffset = 0
-                replace_ranges = []
-
-                def replace_group(match, idx):
-                    nonlocal choffset
-
-                    context['match'] = match
-                    context['idx'] = match.start()
-                    replace_val = None
-
-                    if isinstance(pat.replace_groups, dict):
-                        replace_val = pat.replace_groups.get(str(idx))
-                        if replace_val is None:
-                            group = get_named_group_at_index(match, idx)
-                            if group is not None:
-                                replace_val = pat.replace_groups.get(group)
-                    elif isinstance(pat.replace_groups, list) and idx <= len(pat.replace_groups):
-                        replace_val = pat.replace_groups[idx - 1]
-
-                    if replace_val is None:
-                        return match.group(idx)
-
-                    context['match_cur'] = match.group(idx)
-                    replace_val, colorpos = pyformat.format_string(
-                        replace_val,
-                        context=context,
-                        return_color_positions=True
-                    )
-
-                    colorpos = offset_color_positions(colorpos, match.start(idx))
-                    choffset += len(replace_val) - (match.end(idx) - match.start(idx))
-
-                    update_positions(colorpos, replace_ranges)
-                    replace_ranges.append((
-                        match.span(idx),
-                        (
-                            match.start(idx) + choffset,
-                            match.start(idx) + choffset + len(replace_val)
-                        )
-                    ))
-
-                    update_color_positions(color_positions, colorpos)
-                    return replace_val
-
-                newdata = match_group_replace(pat.regex, data, replace_group)
-                return 'match' in context, newdata
-            return pat.regex.search(data), data
-
-        if pat.replace_all is not None:
-            for field_idx in field_idxs:
-                match = pat.regex.search(fields[field_idx])
-                if match is None:
-                    continue
-
-                context['match'] = match
-                context['idx'] = match.start()
-
-                data, colorpos = pyformat.format_string(
-                    pat.replace_all,
-                    context=context,
-                    return_color_positions=True
-                )
-
-                color_positions.clear()
-                color_positions.update(colorpos)
-                return True, data
-
-        if pat.replace is not None:
-            matched = False
-            for field_idx in field_idxs:
-                newfield, replace_ranges, colorpos = self.pat_schrep(pat, fields[field_idx])
-                if len(replace_ranges) == 0:
-                    continue
-                fields[field_idx] = newfield
-                matched = True
-
-                offset = 0
-                for i in range(field_idx):
-                    offset += len(fields[i])
-
-                for idx in range(len(replace_ranges)): #pylint: disable=consider-using-enumerate
-                    old_range, new_range = replace_ranges[idx]
-                    replace_ranges[idx] = (
-                        (old_range[0] + offset, old_range[1] + offset),
-                        (new_range[0] + offset, new_range[1] + offset),
-                    )
-
-                if offset > 0:
-                    for key in sorted(colorpos.keys(), reverse=True):
-                        colorpos[key + offset] = colorpos[key]
-                        del colorpos[key]
-
-                update_positions(color_positions, replace_ranges)
-                update_color_positions(color_positions, colorpos)
-            if not matched:
-                return False, None
-            return True, ''.join(fields)
-
-        for field_idx in field_idxs:
-            match = pat.regex.search(fields[field_idx])
-            if match is not None:
-                return True, data
-
-        return False, None
-
-    def pat_schrep(self, pattern, string):
-        color_positions = {}
-
-        def replacer(match):
-            newstring, colorpos = pyformat.format_string(
-                pattern.replace,
-                context={
-                    'string': string,
-                    'idx': match.start(),
-                    'color': {
-                        'enabled': self.is_color_enabled(),
-                        'state': self.color_state,
-                        'aliases': self.profloader.color_aliases,
-                        'positions': color_positions
-                    },
-                    'match': match,
-
-                },
-                return_color_positions=True
-            )
-
-            if match.start() > 0:
-                for key in sorted(colorpos.keys(), reverse=True):
-                    colorpos[key + match.start()] = colorpos[key]
-                    del colorpos[key]
-
-            update_color_positions(color_positions, colorpos)
-            return newstring
-
-        newstring, replace_ranges = search_replace(
-            pattern.regex,
-            string,
-            replacer,
-            start_occurrence=pattern.start_occurrence,
-            max_count=pattern.max_count
-        )
-        return newstring, replace_ranges, color_positions
 
     def write_timestamp(self, stream):
         timestamp = '%Y-%m-%d %H:%M:%S: '
