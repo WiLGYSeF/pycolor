71fce4f2cc8d9e5331c92f402dbe486b4d1b0586
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-06-03 21:00:54 -0400

    add is_buffer_empty() test

--- tests/execute_tests/test_execute.py
+++ tests/execute_tests/test_execute.py
@@ -1,4 +1,6 @@
 import datetime
+import io
+import os
 import unittest
 
 import execute
@@ -21,3 +23,29 @@ class ExecuteTest(unittest.TestCase):
         returncode = execute.execute(['date', '+%Y%m%d%H%M%S'], stdout_cb, stderr_cb)
         self.assertEqual(returncode, 0)
         self.assertFalse(recv_stderr)
+
+    def test_is_buffer_empty(self):
+        stream = io.BytesIO()
+        test_data = b'this is a test'
+        output = None
+
+        self.assertTrue(execute.is_buffer_empty(stream))
+
+        stream.write(test_data)
+        stream.seek(0)
+        did_callback = execute.read_stream(stream, lambda x: None)
+        self.assertFalse(did_callback)
+        self.assertFalse(execute.is_buffer_empty(stream))
+        self.assertEqual(execute.read_stream.buffers[stream], test_data)
+
+        def set_output(data):
+            nonlocal output
+            output = data
+
+        stream.write(b'\n')
+        stream.seek(-1, os.SEEK_CUR)
+        did_callback = execute.read_stream(stream, set_output)
+        self.assertTrue(did_callback)
+        self.assertTrue(execute.is_buffer_empty(stream))
+        self.assertTrue(len(execute.read_stream.buffers[stream]) == 0)
+        self.assertEqual(output.encode(), test_data + b'\n')
