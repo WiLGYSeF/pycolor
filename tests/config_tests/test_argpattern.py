fc7c374c29eb117d81cb1ef1becad5798b7ed1f0
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-07-26 07:50:21 -0400

    separate fail test

--- tests/config_tests/test_argpattern.py
+++ tests/config_tests/test_argpattern.py
@@ -20,11 +20,6 @@ GET_ARG_RANGES = [
     },
     {
         ARGLEN: 4,
-        POSITION: 'blah',
-        RANGE: Exception,
-    },
-    {
-        ARGLEN: 4,
         POSITION: '1',
         RANGE: [0],
     },
@@ -80,21 +75,29 @@ GET_ARG_RANGES = [
     },
 ]
 
+GET_ARG_RANGES_FAIL = [
+    {
+        ARGLEN: 4,
+        POSITION: 'blah',
+    },
+]
+
 
 class GetArgPatternTest(unittest.TestCase):
     def test_get_arg_range(self):
         for entry in GET_ARG_RANGES:
-            if isinstance(entry[RANGE], type):
-                with self.assertRaises(entry[RANGE]):
-                    ArgPattern({
-                        'expression': '.*',
-                        'position': entry[POSITION]
-                    }).get_arg_range(entry[ARGLEN])
-            else:
-                self.assertListEqual(
-                    list(ArgPattern({
-                        'expression': '.*',
-                        'position': entry[POSITION]
-                    }).get_arg_range(entry[ARGLEN])),
-                    entry[RANGE]
-                )
+            self.assertListEqual(
+                list(ArgPattern({
+                    'expression': '.*',
+                    'position': entry[POSITION]
+                }).get_arg_range(entry[ARGLEN])),
+                entry[RANGE]
+            )
+
+    def test_get_arg_range_fail(self):
+        for entry in GET_ARG_RANGES_FAIL:
+            with self.assertRaises(Exception):
+                ArgPattern({
+                    'expression': '.*',
+                    'position': entry[POSITION]
+                }).get_arg_range(entry[ARGLEN])
