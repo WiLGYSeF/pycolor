e898ccd0ad3d243877e5f398d19df02fbe6bf79f
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2021-07-26 08:10:28 -0400

    add empty profile_name test

--- tests/config_tests/test_profile.py
+++ tests/config_tests/test_profile.py
@@ -8,6 +8,12 @@ from config.profile import Profile
 
 
 class ProfileTest(unittest.TestCase):
+    def test_profile_name_empty(self):
+        prof = Profile({
+            'profile_name': '',
+        })
+        self.assertIsNone(prof.profile_name)
+
     def test_min_max_args_fail(self):
         with self.assertRaises(ConfigPropertyError):
             Profile({
