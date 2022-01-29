6525d9bfb429edea7b8d02e46d325d9358796247
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-01-29 11:24:16 -0500

    add pylint sample config tests

---
+++ tests/config_tests/sampleconfig_tests/pylint/test_pylint.py
@@ -0,0 +1,21 @@
+import os
+import typing
+import unittest
+
+from tests.config_tests.sampleconfig_tests.helpers import get_sample_config_filename
+from tests.execute_tests.helpers import check_pycolor_execute
+
+MOCKED_DATA = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mocked_data')
+
+class FreeTest(unittest.TestCase):
+    def test_free(self):
+        _do_execute(self, ['pylint'], 'pylint')
+
+def _do_execute(self, args: typing.List[str], test_name: str, **kwargs) -> None:
+    check_pycolor_execute(self,
+        args,
+        MOCKED_DATA,
+        test_name,
+        config_file=get_sample_config_filename('pylint'),
+        **kwargs
+    )
